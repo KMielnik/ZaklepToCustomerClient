8fbcca9efab352f57f5cf7f907fc3bb6211a4954
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-05-03 19:41:27 +0200

    +Main activity

--- app/src/main/java/to/zaklep/zakleptocustomerclient/MainActivity.kt
+++ app/src/main/java/to/zaklep/zakleptocustomerclient/MainActivity.kt
@@ -1,12 +1,28 @@
 package to.zaklep.zakleptocustomerclient
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onSignUpButtonClicked(view: View) {
+
+
+    }
+
+    fun onSignInButtonClicked(view: View) {
+
+    }
+
+    fun onGuestButtonClicked(view: View) {
+
     }
 }
