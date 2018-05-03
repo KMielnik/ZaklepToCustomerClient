a767f1c218edd9d09ec164f39993a1210a789ae9
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-05-04 00:51:10 +0200

    Created sliding filter from top of the screen

--- app/src/main/java/to/zaklep/zakleptocustomerclient/BrowseActivity.kt
+++ app/src/main/java/to/zaklep/zakleptocustomerclient/BrowseActivity.kt
@@ -1,17 +1,24 @@
 package to.zaklep.zakleptocustomerclient
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.graphics.drawable.AnimationUtilsCompat
+import android.support.v4.view.GestureDetectorCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_browse.*
 import kotlinx.android.synthetic.main.app_bar_browse.*
+import kotlinx.android.synthetic.main.content_browse.*
 
 class BrowseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    var isHiddenPanelShown: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,39 @@ class BrowseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    var downY: Float = 0.0f
+    var startClickTime: Long = 0
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            downY = ev.y
+            startClickTime = System.currentTimeMillis()
+        } else if (ev?.action == MotionEvent.ACTION_UP) {
+            if (System.currentTimeMillis() - startClickTime > ViewConfiguration.getTapTimeout() / 2)
+                if (ev.y > downY - 10) {
+                    if (!isHiddenPanelShown) {
+                        var slideDown = AnimationUtils.loadAnimation(this, R.anim.abc_slide_in_top)
+                        hidden_panel.startAnimation(slideDown)
+                        hidden_panel.visibility = View.VISIBLE
+                        placeForFilter.startAnimation(slideDown)
+                        placeForFilter.visibility = View.VISIBLE
+                        isHiddenPanelShown = true
+                    }
+                } else {
+                    if (isHiddenPanelShown) {
+                        var slideUp = AnimationUtils.loadAnimation(this, R.anim.abc_slide_out_top)
+                        hidden_panel.startAnimation(slideUp)
+                        hidden_panel.visibility = View.GONE
+                        placeForFilter.startAnimation(slideUp)
+                        placeForFilter.visibility = View.GONE
+                        isHiddenPanelShown = false
+                    }
+                }
+        }
+
+        return super.dispatchTouchEvent(ev)
     }
 
     override fun onBackPressed() {
