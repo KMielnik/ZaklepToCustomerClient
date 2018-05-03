85198f4c9bdeead1eb3b458cd561ee87cdb77796
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-05-03 21:18:05 +0200

    +Browse Activity (not finished)

---
+++ app/src/main/java/to/zaklep/zakleptocustomerclient/CitySelectionActivity.kt
@@ -0,0 +1,18 @@
+package to.zaklep.zakleptocustomerclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_city_selection.*
+
+class CitySelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_selection)
+    }
+
+    fun onCitySelectButtonClicked(view: View) {
+
+    }
+}
